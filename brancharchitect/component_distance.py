89f2e6db8b2826ef4d308691e1e2f43ef465c463
syntonym <syntaar@gmail.com>
2024-07-12 16:55:03 +0200

    implement jump path distance

--- brancharchitect/component_distance.py
+++ brancharchitect/component_distance.py
@@ -1,37 +1,74 @@
 from brancharchitect.tree import Node, SplitIndices
+from collections import Counter
 from typing import Tuple, List
 
+class ComponentTranslator:
 
-def component_distance(tree1: Node, tree2: Node, components: List[tuple[str, ...]], weighted: bool=False) -> dict[tuple[str, ...], float]:
-    names_to_splits = {c: tree1._index(c) for c in components}
-    # string component can be "in the wrong order", so we need to remember the original order
-    splits_to_names = {s: name for name, s in names_to_splits.items()}
-    components = [names_to_splits[component] for component in components]
+    def __init__(self, tree):
+        self.tree = tree
+        self.r_cache = {}
 
-    distances: dict[SplitIndices, float] = {}
+    def to_idx(self, c):
+        idx = self.tree._index(c)
+        self.r_cache[idx] = c
+        return idx
+
+    def to_str(self, c):
+        return self.r_cache[c]
+
+
+def component_distance(tree1: Node, tree2: Node, components: List[tuple[str, ...]], weighted: bool=False) -> list[float]:
+    translator = ComponentTranslator(tree1)
+    components = [translator.to_idx(c) for c in components]
+    distances = []
 
     for component in components:
         d1 = jump_distance(tree1, tree2.to_splits(), component, weighted=weighted)
         d2 = jump_distance(tree2, tree1.to_splits(), component, weighted=weighted)
-        distances[component] = (d1 + d2)
-
-    translated_distances = {splits_to_names[split]: value for split, value in distances.items()}
-    return translated_distances
+        distances.append(d1+d2)
+    return distances
 
 
-def jump_distance(node: Node, reference: Node, component: SplitIndices, distance=0, weighted=False) -> int:
-
-    if node.split_indices in reference:
-        distance = 0
+def jump_distance(node: Node, reference: Node, component: SplitIndices, weighted=False) -> float:
+    path = jump_path(node, reference, component)
+    if weighted:
+        return sum(node.length for node in path)
     else:
-        if weighted:
-            distance += node.length
+        return len(path)
+
+
+def jump_path(node, reference, component):
+    path = []
+    while set(node.split_indices) != set(component):
+        if node.split_indices in reference:
+            path = []
         else:
-            distance += 1
+            path.append(node)
+        for child in node.children:
+            if set(component) <= set(child.split_indices):
+                node = child
+                break
+        else:
+            break
+            # Component is actually not a component 
+    return path
 
-    for child in node.children:
-        if set(component) < set(child.split_indices):
-            #only one child can contain the component
-            return jump_distance(child, reference, component, distance, weighted=weighted)
 
-    return distance
+def jump_path_distance(tree1: Node, tree2: Node, components: List[tuple[str, ...]], weighted:bool = False) -> dict[tuple[str, ...], float]:
+    translator = ComponentTranslator(tree1)
+    components = [translator.to_idx(c) for c in components]
+
+    paths1 = [jump_path(tree1, tree2.to_splits(), component) for component in components]
+    paths2 = [jump_path(tree2, tree1.to_splits(), component) for component in components]
+
+    counter = Counter(node.split_indices for path in paths1+paths2 for node in path)
+
+    distances = []
+    for p1, p2 in zip(paths1, paths2):
+        print([n.split_indices for n in p1], [n.split_indices for n in p2])
+        if weighted:
+            d = sum(n.length / counter[n.split_indices] for n in p1+p2)
+        else:
+            d = sum(1 / counter[n.split_indices] for n in p1+p2)
+        distances.append(d)
+    return distances
--- test/test_component_distance.py
+++ test/test_component_distance.py
@@ -1,12 +1,13 @@
 from brancharchitect.newick_parser import parse_newick
-from brancharchitect.component_distance import component_distance
+from brancharchitect.component_distance import component_distance, jump_path_distance
 from brancharchitect.distances import robinson_foulds_distance
+from brancharchitect.jumping_taxa.bruteforce_algorithm import algorithm as brute_force_algo
 
 
 def test_component_distance():
     trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);" +"(((6:1,(X:1,5:1)),(1:1,2:1):1),O:1);")
     components = [('X',)]    
-    expected_distance = {('X',): 4}
+    expected_distance = [4]
 
     observed_distance = component_distance(trees[0], trees[1], components)
 
@@ -16,7 +17,7 @@ def test_component_distance():
 def test_two_component_distance():
     trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);" + "(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);")
     components = [('X',)]    
-    expected_distance = {('X',): 0}
+    expected_distance = [0]
 
     observed_distance = component_distance(trees[0], trees[1], components)
 
@@ -26,7 +27,7 @@ def test_two_component_distance():
 def test_cherry_is_hopping_component_distance():
     trees = parse_newick("(((6:1,5:1),((1:1,2:1):1,(X:1,Y:1):1):1):1,O:1);" + "(((6:1,5:1),((1:1,2:1):1,(X:1,Y:1):1):1):1,O:1);")
     components = [('X', 'Y')]    
-    expected_distance = {('X', 'Y'): 0}
+    expected_distance = [0]
 
     observed_distance = component_distance(trees[0], trees[1], components)
 
@@ -36,33 +37,17 @@ def test_cherry_is_hopping_component_distance():
 def test_taxa_switch_from_outgroup():
     trees = parse_newick("(((A:1,B:1),(C:1,D:1)),X:1);" + "(((X:1,B:1):1,(C:1,D:1):1),A:1);")
     components = [('X',)]    
-    expected_distance = {('X',): 2}
+    expected_distance = [2]
 
     observed_distance = component_distance(trees[0], trees[1], components)
 
     assert expected_distance == observed_distance
 
 
-def test_cherry_is_hopping_component():
-    trees = parse_newick("(((6:1,5:1),(1:1,(2:1,(X:1,Y:1)):1):1):1,O:1);" +"(((6:1,((X:1,Y:1),5:1)),(1:1,2:1):1),O:1);")
-    components = [('X','Y'), ('1', '2'), ('6', '5')]    
-    expected_distance = {
-            ('X','Y'): 4,
-            ('1','2'): 1,
-            ('6', '5'): 1
-        }
-    
-    rfd = robinson_foulds_distance(trees[0], trees[1])    
-    observed_distances = component_distance(trees[0], trees[1], components)
-
-    assert expected_distance == observed_distances
-    assert rfd == 6/2
-
-
 def test_weighted_component_distance():
     trees = parse_newick("(((6,5),(1,(2,X):2)),O);" +"(((6,(X,5)),(1,2)),O);")
     components = [('X',)]    
-    expected_distance = {('X',): 5}
+    expected_distance = [5]
 
     observed_distance = component_distance(trees[0], trees[1], components, weighted=True)
 
@@ -72,7 +57,7 @@ def test_weighted_component_distance():
 def test_same_tree():
     trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):2):1):1,O:1);" +"(((6:1,5:1),(1:1,(2:1,X:1):2):1):1,O:1);")
     components = [('X',)]    
-    expected_distance = {('X',): 0}
+    expected_distance = [0]
 
     observed_distance = component_distance(trees[0], trees[1], components, weighted=True)
 
@@ -82,8 +67,78 @@ def test_same_tree():
 def test_zero_edge_high_in_hierarchy():
     trees = parse_newick('(((1,2),(3,4)),5);' + '((2,(3,4)),(5,1));')
     components = [('4',), ('1',)]    
-    expected_distance = {('4',): 0, ('1',): 3}
+    expected_distance = [0, 3]
 
     observed_distance = component_distance(trees[0], trees[1], components)
+
+    assert expected_distance == observed_distance
+
+
+def test_jump_path_distance():
+    trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);" +"(((6:1,(X:1,5:1)),(1:1,2:1):1),O:1);")
+    components = [('X',)]    
+    expected_distance = [4]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components)
+
+    assert expected_distance == observed_distance
+
+
+def test_two_jump_path_distance():
+    trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);" + "(((6:1,5:1),(1:1,(2:1,X:1):1):1):1,O:1);")
+    components = [('X',)]    
+    expected_distance = [0]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components)
+
+    assert expected_distance == observed_distance
+
+
+def test_cherry_is_hopping_jump_path_distance():
+    trees = parse_newick("(((6:1,5:1),((1:1,2:1):1,(X:1,Y:1):1):1):1,O:1);" + "(((6:1,5:1),((1:1,2:1):1,(X:1,Y:1):1):1):1,O:1);")
+    components = [('X', 'Y')]    
+    expected_distance = [0]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components)
+
+    assert expected_distance == observed_distance
+
+    
+def test_taxa_switch_from_outgroup_jump_path_distance():
+    trees = parse_newick("(((A:1,B:1),(C:1,D:1)),X:1);" + "(((X:1,B:1):1,(C:1,D:1):1),A:1);")
+    components = [('X',)]    
+    expected_distance = [2]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components)
+
+    assert expected_distance == observed_distance
+
+
+def test_weighted_jump_path_distance():
+    trees = parse_newick("(((6,5),(1,(2,X):2)),O);" +"(((6,(X,5)),(1,2)),O);")
+    components = [('X',)]    
+    expected_distance = [5]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components, weighted=True)
+
+    assert expected_distance == observed_distance
+
+
+def test_same_tree_jump_path_distance():
+    trees = parse_newick("(((6:1,5:1),(1:1,(2:1,X:1):2):1):1,O:1);" +"(((6:1,5:1),(1:1,(2:1,X:1):2):1):1,O:1);")
+    components = [('X',)]    
+    expected_distance = [0]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components, weighted=True)
+
+    assert expected_distance == observed_distance
+
+
+def test_zero_edge_high_in_hierarchy_jump_path_distance():
+    trees = parse_newick('(((1,2),(3,4)),5);' + '((2,(3,4)),(5,1));')
+    components = [('4',), ('1',)]    
+    expected_distance = [0, 3]
+
+    observed_distance = jump_path_distance(trees[0], trees[1], components)
 
     assert expected_distance == observed_distance
