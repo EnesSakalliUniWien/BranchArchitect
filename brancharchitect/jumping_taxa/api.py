b01f3d378124ed85b1ef6eef0992190f30f5f4be
syntonym <syntaar@gmail.com>
2024-07-25 11:46:26 +0200

    make algorithm selectable in API

--- brancharchitect/jumping_taxa/api.py
+++ brancharchitect/jumping_taxa/api.py
@@ -1,13 +1,27 @@
 from brancharchitect.jumping_taxa.tree_interpolation import interpolate_tree
-from brancharchitect.jumping_taxa.algorithm_five import algorithm_five
 from brancharchitect.tree import Node
 
-def call_jumping_taxa(tree1 : Node, tree2: Node):
+def call_jumping_taxa(tree1 : Node, tree2: Node, algorithm='rule'):
+
+    import brancharchitect.jumping_taxa.algorithm_five
+    import brancharchitect.jumping_taxa.algo_new
+    import brancharchitect.jumping_taxa.bruteforce_algorithm
+    ALGORITHMS = {
+                  'rule': brancharchitect.jumping_taxa.algorithm_five.algorithm_five,
+                  'set' : brancharchitect.jumping_taxa.algo_new.algorithm,
+                  'bruteforce': brancharchitect.jumping_taxa.bruteforce_algorithm.algorithm
+                 }
+
     if tree1._order != tree2._order:
         raise ValueError('Trees have incompatible leaf order')
+    if algorithm not in ALGORITHMS:
+        raise ValueError(f'algorithm {algorithm} not supported, only supported values are {list(ALGORITHMS.keys())}')
 
     it1, c1, c2, it2 = interpolate_tree(tree1, tree2)
 
-    jumping_taxa = algorithm_five(it1, it2, tree1._order)
+    f = ALGORITHMS[algorithm]
+
+    jumping_taxa = f(it1, it2, tree1._order)
+    print(jumping_taxa)
     jumping_taxa = [tuple(tree1._order[i] for i in idx) for idx in jumping_taxa]
     return jumping_taxa
--- brancharchitect/jumping_taxa/bruteforce_algorithm.py
+++ brancharchitect/jumping_taxa/bruteforce_algorithm.py
@@ -42,54 +42,92 @@ def _get_components(node, s):
         is_component.append(is_c)
         components.extend(cs)
 
-    if all(is_component) and node.split_indices in s:
+    if node.split_indices in s and all(is_component):
             return [node.split_indices], True
     return components, False
+
+
+def reduce(components, others):
+    r = []
+    for component in components:
+        for other in others:
+            if other == component:
+                continue
+            if contains(component, other):
+                break
+        else:
+            r.append(component)
+    return r
 
 
 def get_components(t1, t2):
     s1 = set(t1.to_splits())
     s2 = set(t2.to_splits())
-    s = s1 & s2
-
-    c1, _ = _get_components(t1, s)
-    c2, _ = _get_components(t2, s)
+    c1, _ = _get_components(t1, s2)
+    c2, _ = _get_components(t2, s1)
 
     c1 = reduce(c1, c2)
     c2 = reduce(c2, c1)
     components = list(set(c1) | set(c2))
+
     return components
 
 
-def algorithm(t1, t2, _, timeout=60*60):
-    components = get_components(t1, t2)
-    jts = core(t1, t2, components, timeout)
-    return jts
+def algorithm(t1, t2, _=None, timeout=60*60):
+    jts = core(t1, t2, timeout)
+    sorted(jts, key=lambda solution: sum(len(component) for component in solution))
+    return jts[0]
 
 
-def core(t1, t2, components, timeout=60*60):
+def core(t1, t2, timeout=60*60, max_depth=None):
     leaves = [c for c in traverse(t1) if len(c.children) == 0]
+    if max_depth is None:
+        max_depth = int(len(leaves) / 2)
 
     jumping_taxa = []
     depth = 1
 
     start = time.time()
 
-    while len(jumping_taxa) == 0 and depth < int(len(leaves)/2):
-        i = 0
+    stack = [(t1, t2, [0], [get_components(t1, t2)])]
+    i = 0
+    while len(jumping_taxa) == 0 and depth < max_depth:
 
-        for candidates in combinations(components, depth):
-            to_remove = [i for c in candidates for i in c]
-            it1 = delete_taxa(t1.deep_copy(), to_remove)
-            it2 = delete_taxa(t2.deep_copy(), to_remove)
+        while(len(stack)) > 0:
 
-            if trees_equal(it1, it2):
-                jumping_taxa.append(candidates)
+            t1, t2, idxs, cs = stack.pop()
+
+            li = idxs[-1]
+            lc = cs[-1]
+
+            if li >= len(lc):
+                if len(stack) >= 1:
+                    t1, t2, idxs, cs = stack.pop()
+                    stack.append((t1, t2, idxs[:-1] + [idxs[-1]+1], cs))
+            elif len(idxs) < depth:
+                stack.append((t1, t2, idxs, cs))
+
+                to_remove = lc[li]
+                it1 = delete_taxa(t1.deep_copy(), to_remove)
+                it2 = delete_taxa(t2.deep_copy(), to_remove)
+
+                stack.append((it1, it2, idxs + [0], cs + [get_components(it1, it2)]))
+            else:
+                to_remove = lc[li]
+                it1 = delete_taxa(t1.deep_copy(), to_remove)
+                it2 = delete_taxa(t2.deep_copy(), to_remove)
+
+                if trees_equal(it1, it2):
+                    jumping_taxa.append(list(tuple([t for t in c[i]]) for c, i in zip(cs, idxs)))
+
+                stack.append((t1, t2, idxs[:-1] + [idxs[-1]+1], cs))
 
             i += 1
             if i % 1000 == 0:
                 if time.time() - start >= timeout:
                     raise ValueError('too much time, giving up')
+
         depth += 1
+        stack = [(t1, t2, [0], [get_components(t1, t2)])]
     return jumping_taxa
 
